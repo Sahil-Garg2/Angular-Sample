75c7c64b4fd92ce9d8d1d30b6f3af847cbbcaffc
null <null>
2025-01-06 11:03:15 +0530

    Sample testing

---
+++ src/app/sample-test/sample-test.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-sample-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './sample-test.component.html',
+  styleUrl: './sample-test.component.css'
+})
+
+
+export class SampleTestComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import {SampleTestComponent} from './app/sample-test/sample-test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   template: `
+  
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
+    <app-sample-test> </app-sample-test>
   `,
 })
 export class App {
-  name = 'Angular';
+  name = 'Sahil';
 }
 
 bootstrapApplication(App);
